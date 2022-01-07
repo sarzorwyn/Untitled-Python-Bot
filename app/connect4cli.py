a3e6aeb5345b86ad23635b130f66e546b7ea4ff6
hussain1998 <e0406406@u.nus.edu>
2022-01-07 20:15:22 +0800

    bugfixes

--- app/connect4cli.py
+++ app/connect4cli.py
@@ -4,7 +4,7 @@ game = ConnectFour.new_game()
 player = 1
 while True:
     print(game.board_to_emoji())
-    print(game.board)
+    print(game.active_player,game.board)
     col = int(input("Enter a column number"))
     if game.play_turn(col):
         break
