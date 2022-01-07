cdd343e968a12f87b50d046c92a07758daa49e01
Ivan <ivan.t.mailbox@gmail.com>
2022-01-07 17:18:06 +0800

    Connect four skeleton

---
+++ connect_four/connectFour.py
@@ -0,0 +1,8 @@
+
+class ConnectFourGame:
+
+    def __init__(self, board, num_turns, game_won):
+        self.board = board # 2d array
+        self.num_turns = num_turns
+        self.games_won = game_won
+
