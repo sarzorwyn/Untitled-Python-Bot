620356c2e24a5876a310cf7e7657e4bdcf2e7509
Ivan <ivan.t.mailbox@gmail.com>
2022-01-08 11:45:00 +0800

    Added instructions to create config file

---
+++ app/config_template.py
@@ -0,0 +1,26 @@
+"""
+Rename this file to config.py and fill up the api, bearer and access keys.
+"""
+
+class Config(object):
+    DEBUG = True
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    SECRET_KEY = "this-is-a-super-secret-key"
+
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': DevelopmentConfig,
+    'production': DevelopmentConfig
+}
+
+
+
+API_KEY = ''
+API_KEY_SECRET = ''
+BEARER_TOKEN = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
